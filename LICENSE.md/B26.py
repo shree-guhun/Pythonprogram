be3a3ce3b55e882256ffb75b577580974b22ad2f
shree-guhun <36299259+shree-guhun@users.noreply.github.com>
2018-05-20 22:53:55 +0530

    Update B26.py

--- LICENSE.md/B26.py
+++ LICENSE.md/B26.py
@@ -1,16 +1,11 @@
-def findMean(a, n1):
-    sum = 0
-    for i in range( 0, n1):
-        sum += a[i]
-     
-    return float(sum/n1)
-def findMedian(a, n1):
-    sorted(a)
-    if n1 % 2 != 0:
-        return float(a[n1/2])
-     
-    return float((a[int((n1-1)/2)] +
-                  a[int(n1/2)])/2.0)
-a = [ 2,3,1 ]
-n1 = len(a)
-print("Median =", findMedian(a, n1))
+n1=int(input("enter n"))
+a=0
+b=a
+l=[]
+for i in range(0,n1):
+    a=int(input())
+    l.append(a)
+    l.sort()
+    if i==2:
+        b=l[i]
+print(b)
