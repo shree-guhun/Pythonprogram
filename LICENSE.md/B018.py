6bcdc5d1725ca8d876d1f8a7914b8d15b2e6cf13
shree-guhun <36299259+shree-guhun@users.noreply.github.com>
2018-05-20 22:04:08 +0530

    Update and rename B18.py to B018.py

--- LICENSE.md/B018.py
+++ LICENSE.md/B018.py
@@ -1,11 +1,11 @@
 l=int(input())
 u=int(input())
-for N in range(l,u):
+for N1 in range(l,u):
   sum=0
-  temp=N
+  temp=N1
   while temp>0:
     digit=temp%10
     sum+=digit**3
     temp//=10
-  if N==sum:
-   print(N)
+  if N1==sum:
+   print(N1)
