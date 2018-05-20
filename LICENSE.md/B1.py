e27a3a1c67f1ecb21fb95aad78ef1ccc4446fc1b
shree-guhun <36299259+shree-guhun@users.noreply.github.com>
2018-05-20 19:25:42 +0530

    B1.py

---
+++ LICENSE.md/B1.py
@@ -0,0 +1,9 @@
+n=float(input("Enter the number:"))
+if(1<=n<=100000):
+print("The given number is positive")
+elif(n==0):
+print("The given number is zero")
+elif(n<0):
+print("The given number is negative")
+elif(n>100000):
+print("The given number is invalid")
