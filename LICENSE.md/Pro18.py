30f0797fc3868354048905afeeb521760b87333f
shree-guhun <36299259+shree-guhun@users.noreply.github.com>
2018-05-21 08:41:02 +0530

    Pro18.py

---
+++ LICENSE.md/Pro18.py
@@ -0,0 +1,35 @@
+def printMaxSubSquare(M):
+    R = len(M)
+    print(R)
+    C = len(M[0]
+    print(C)
+ 
+    S = [[0 for k in range(C)] for l in range(R)]
+    for i in range(1, R):
+        for j in range(1, C):
+            if (M[i][j] == 1):
+                S[i][j] = min(S[i][j-1], S[i-1][j],
+                            S[i-1][j-1]) + 1
+            else:
+                S[i][j] = 0
+    max_of_s = S[0][0]
+    max_i = 0
+    max_j = 0
+    for i in range(R):
+        for j in range(C):
+            if (max_of_s < S[i][j]):
+                max_of_s = S[i][j]
+                max_i = i
+                max_j = j
+    print("Maximum size sub-matrix is: ")
+    for i in range(max_i, max_i - max_of_s, -1):
+        for j in range(max_j, max_j - max_of_s, -1):
+            print (M[i][j], end = " ")
+        print("")
+M = [[0, 1, 1, 0, 1],
+    [1, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0]]
+printMaxSubSquare(M)
